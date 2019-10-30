979d98050fc083d9cc5c6c6dfebaccc17717bf3e
zhonggd3 <zhonggd3@chinaunicom.cn>
2019-10-30 14:38:11 +0800

    SpringBoot

---
+++ jelly/src/main/java/com/sdjn/jelly/JellyApplication.java
@@ -0,0 +1,13 @@
+package com.sdjn.jelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JellyApplication.class, args);
+    }
+
+}
---
+++ jelly/src/test/java/com/sdjn/jelly/JellyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdjn.jelly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
